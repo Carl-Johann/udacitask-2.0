a8cfe0f12481c16e2a3d0e329d0bad289205fc14
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-23 22:04:34 +0200

    if the priotity value entered isn't one of the four, it will throw an error

--- listable.rb
+++ listable.rb
@@ -1,4 +1,6 @@
+require_relative 'errors'
 module Listable
+  include UdaciListErrors
   
   def format_description(description)
     "#{description}".ljust(30)
@@ -16,12 +18,16 @@ module Listable
   end
 
   def format_priority(priority)
-  	value = " ⇧" if priority == "high"
-    value = " ⇨" if priority == "medium"
-    value = " ⇩" if priority == "low"
-    value = "" if !priority
-    return value
-  end
+  	if ["high", "medium", "low", nil].include?(priority)
+  		value = " ⇧" if priority == "high"
+   		value = " ⇨" if priority == "medium"
+   		value = " ⇩" if priority == "low"
+   		value = "" if !priority
+   		return value
+   	else 
+   		raise InvalidPriorityValue, "'#{priority}' is not a valid priority value'"
+   	end
+   end
 end
 
 
