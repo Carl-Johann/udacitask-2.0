e383dc1f85187a175fc021e1ed321e47e34200c8
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-23 21:55:11 +0200

    i changed out the 'Date' with 'Chronic' so that i can use the gem chronic

--- todo.rb
+++ todo.rb
@@ -1,16 +1,18 @@
 require_relative 'listable'
+require_relative 'errors'
 class TodoItem
   include Listable
+  include UdaciListErrors  
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
 
   def details
-    format_description(@description) + "due: " +
+    format_description(@description) + "Due: " +
     format_date(due:@due) +
     format_priority(@priority)
   end
