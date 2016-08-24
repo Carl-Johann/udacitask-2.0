df8460549bb7ad3c6f509a676e9046fbc9f04d37
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-24 20:17:35 +0200

    added the 'new_priority' method so the priorities can now be changed

--- todo.rb
+++ todo.rb
@@ -3,16 +3,21 @@ require_relative 'errors'
 class TodoItem
   include Listable
   include UdaciListErrors  
-  attr_reader :description, :due, :priority
+  attr_reader :description, :due, :priority, :type
 
-  def initialize(description, options={})
+  def initialize(description, options={}, type)
     @description = description
     @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
+    @type = type.capitalize
+  end
+
+  def new_priority(new_priority)
+    @priority = new_priority
   end
 
   def details
-    format_description(@description) + "Due: " +
+    format_description(@description, @type) + "Due: " +
     format_date(due:@due) +
     format_priority(@priority)
   end
