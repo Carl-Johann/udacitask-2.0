e5cae174464240f71ff9e4c17aeaa20e06dc4981
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-24 20:12:53 +0200

    Minor tweeks

--- event.rb
+++ event.rb
@@ -1,15 +1,20 @@
 require_relative 'listable'
 class EventItem
   include Listable
-  attr_reader :description, :start_date, :end_date
+  attr_reader :description, :start_date, :end_date, :type
 
-  def initialize(description, options={})
+  def initialize(description, options={}, type)
     @description = description
     @start_date = Date.parse(options[:start_date]) if options[:start_date]
     @end_date = Date.parse(options[:end_date]) if options[:end_date]
+    @type = type.capitalize
+  end
+
+  def new_priority(new_priority)
+    @priority = new_priority
   end
 
   def details
-    format_description(@description) + "event dates: " + format_date(start_date:@start_date, end_date:@end_date)
+    format_description(@description, @type) + "Event Dates: " + format_date(start_date:@start_date, end_date:@end_date)
   end
 end
