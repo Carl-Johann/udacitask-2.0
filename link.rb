3b40703032407bdbc21ee85c08304b81bb1094ed
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-24 20:14:43 +0200

    If no 'site_name' value  is entered, the standard value is:'No Site Name'

--- link.rb
+++ link.rb
@@ -1,18 +1,23 @@
 require_relative 'listable'
 class LinkItem
   include Listable
-  attr_reader :description, :site_name
+  attr_reader :description, :site_name, :type
 
-  def initialize(url, options={})
+  def initialize(url, options={}, type)
     @description = url
     @site_name = options[:site_name]
+    @type = type.capitalize
   end
 
   def format_name
-    @site_name ? @site_name : ""
+    @site_name ? @site_name : "'No Site Name'"
   end
 
+  def new_priority(new_priority)
+    @priority = new_priority
+  end
+  
   def details
-    format_description(@description) + "Site Name: " + format_name
+    format_description(@description, @type) + "Site Name: " + format_name
   end
 end
